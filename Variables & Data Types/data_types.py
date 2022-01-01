99235163b6319b8d4462ebbbd4686cae921c561a
antonygatua <antonnymuiko@gmail.com>
2022-01-01 21:26:05 +0300

    Getting started

---
+++ Variables & Data Types/data_types.py
@@ -0,0 +1,16 @@
+# strings - series of characters
+# Anything in quotes is considered a string in python
+
+"This is a string"
+'This also is a string'
+
+# Using apostrophes and quotes
+'I told my friend, "Python is my favourite language!"'
+"One of Python's strengths is its diverse and supportive community."
+
+# changing case in a string with methods
+# Method - an action that python can perform on a piece of data
+name = "antonny muiko"
+print(name.title()) # makes the first letter capital
+print(name.upper()) # all uppercase
+print(name.lower()) # all lower
---
+++ Variables & Data Types/hello_world.py
@@ -0,0 +1,13 @@
+# variables - object
+# add a variable called message and asign text value
+message = "Hello Python world"
+print(message)
+
+new = "Python Refersher course"
+print(new)
+
+# Variable names can only contain letter, numbers and underscores
+# can start with a letter or underscore but not with a number
+# spaces are not allowed in variable names, but underscores can be used to separate words in variable names
+# Avoid using key words and function names as variable names eg print
+# variable names should be short but descriptive eg student_name is better than s_n
