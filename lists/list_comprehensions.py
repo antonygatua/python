d151c21470c2cd70034ec2758bbb3d598d9a0dd9
antonygatua <antonnymuiko@gmail.com>
2022-01-04 23:05:20 +0300

    getting started with list comprehensions

---
+++ lists/list_comprehensions.py
@@ -0,0 +1,8 @@
+# list comprehenison allows you to generate a list with just one line of code
+squares = [value**2 for value in range(1,11)]
+print(squares)
+
+# Try it yourself 
+# Cube comprehension: Use a list comprehension to generate a list of the first 10 cubes
+cubes = [value**3 for value in range(1,11)]
+print(cubes)
