b777770af4a6eba45eb760f9dc2f74e66fc6b2c8
antonygatua <antonnymuiko@gmail.com>
2022-01-04 13:32:20 +0300

    generating numbers with Range

--- lists/Range/first_numbers.py
+++ lists/Range/first_numbers.py
@@ -7,3 +7,6 @@ for value in range(1, 5): # only numbers 1 through 4
 # convert the results of range() directly into a list using the list() function
 numbers = list(range(1,6))
 print(numbers)
+
+# Note: range() format - range(start, stop, step) 
+# Default step is 1 
