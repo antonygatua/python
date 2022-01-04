ef321c2bfc661119f10a555d4bf3393c9657a488
antonygatua <antonnymuiko@gmail.com>
2022-01-05 00:58:38 +0300

    slicing lists

--- lists/slicing.py
+++ lists/slicing.py
@@ -18,3 +18,21 @@ print("Here are the first three players on my team:")
 for player in players[:3]:
     print(player.title())
 
+# try it yourself
+# Slice: Create a list of your own 
+nba = ["lakers", "celtics", "warriors", "clippers", "bucks", "heat", "nets"]
+
+# print the first three teams are
+print("\nThe first three teams are:")
+for team in nba[0:3]:
+    print(team.title())
+
+# print three items from the middle of the list
+print("\nTeams in the middle of the list are:")
+for team in nba[2:5]:
+    print(team.title())
+
+# print the last three items in the list
+print("\nLast three items in the list are:")
+for team in nba[-3:]:
+    print(team.title())
