44bde6a107bcbd74075cbfd278abf36656c75d60
antonygatua <antonnymuiko@gmail.com>
2022-01-08 23:47:03 +0300

    Getting started with dictionaries

---
+++ Dictionaries/alien.py
@@ -0,0 +1,9 @@
+# Dictionary is a collection of key-value pairs. 
+# Each key is connected to a value and can use a key to access the value associated with that key
+# disctionary is wrapped in {} with key separated from values using a :
+# key-value pair are separated by a comma
+
+alien_0 = {'color': 'green', 'points':5}
+
+print(alien_0['color'])
+print(alien_0['points'])
