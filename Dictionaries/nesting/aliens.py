922f99bf9e2929f5eb1bf515d3cc1ef544c7c1c9
antonygatua <antonnymuiko@gmail.com>
2022-01-12 23:27:13 +0300

    nesting dictionary in list

--- Dictionaries/nesting/aliens.py
+++ Dictionaries/nesting/aliens.py
@@ -13,3 +13,21 @@ aliens = [alien_0, alien_1, alien_2]
 # loop through the list and print out each alien
 for alien in aliens:
     print(alien)
+
+# Try it userself
+# People - Create a dictionary representing of people with first_name, last_name, age and city
+# make more dictionaries representing different people, store all three dictionaries in a list called people
+# loop through the list of people and print 
+person_0 = {'first_name': 'antonny', 'second_name': 'muiko', 'age': 15, 'city': 'kitale'}
+person_1 = {'first_name': 'rose', 'second_name': 'shiro', 'age': 20, 'city': 'nanyuki'}
+person_2 = {'first_name': 'yvonne', 'second_name': 'gachara', 'age': 21, 'city': 'nairobi'}
+
+people = [person_0, person_1, person_2]
+
+for person in people:
+    for user, user_info in person.items():
+        if type(user_info) == int:
+            print(f"{user.title()}: {user_info}")
+        else:
+            print(f"{user.replace('_',' ').title()}: {user_info.title()}")
+    print("\n")
