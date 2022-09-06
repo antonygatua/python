bbe4b835c68e79e03ab24a7c784eb6a2df865959
antonygatua <antonnymuiko@gmail.com>
2022-09-06 18:13:21 +0300

    Passing a list

---
+++ functions/greet_users.py
@@ -0,0 +1,10 @@
+# Passing a list 
+
+def greet_users(names):
+    """Print a simple greeting to each user in the list"""
+    for name in names:
+        msg = f"Hello, {name.title()}!"
+        print(msg)
+
+user_names = ["hannah", "ty", "margot"]
+greet_users(user_names)
