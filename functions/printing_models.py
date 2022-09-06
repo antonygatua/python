647b42d2029e4dd0b8895992d86988661632b9d7
antonygatua <antonnymuiko@gmail.com>
2022-09-06 18:14:11 +0300

    modifying a list in a function

---
+++ functions/printing_models.py
@@ -0,0 +1,40 @@
+# modifying a list in a function 
+"""
+Consider a company that creates 3D printed models of designs that
+users submit. Designs that need to be printed are stored in a list, and after
+being printed they’re moved to a separate list
+
+"""
+
+def print_models(unprinted_designs, completed_models):
+    """
+    Simulate printing each design, until none are left
+    Move each design to completed_models after printing
+    """
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        print(f"Printing model: {current_design}")
+        completed_models.append(current_design)
+    
+def show_completed_models(completed_models):
+    """Show all the models that were printed."""
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
+
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
+
+# passing the function a copy of the list not the original
+# any changes that the function makes to the list will affect only the copy
+# leaving the original list intact
+# function_name(list_name[:])
+
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+print_models(unprinted_designs[:], completed_models)
+show_completed_models(completed_models)
