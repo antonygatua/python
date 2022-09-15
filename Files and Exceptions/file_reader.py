4fabe20d70c6a0597bb85c3acb6028532babec4e
antonygatua <antonnymuiko@gmail.com>
2022-09-16 00:29:29 +0300

    Reading a file

---
+++ Files and Exceptions/file_reader.py
@@ -0,0 +1,15 @@
+# reading an entire file
+"""Program that opens the file pi_digits.txt
+Reads the file and prints the contents of the file to the screen
+"""
+
+with open('pi_digits.txt') as file_object:
+    contents = file_object.read()
+print(contents)
+
+# making a list of lines from a file
+with open('pi_digits.txt') as file_object:
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.rstrip())
