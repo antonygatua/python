d8bb08c41cbc72d1d03c205bed76838f92128176
antonygatua <antonnymuiko@gmail.com>
2024-02-20 19:58:07 +0300

    type casting

---
+++ The Basics/Files and Exceptions/typecasting.py
@@ -0,0 +1,31 @@
+a = 7 
+print(type(a))
+
+b = 5.0
+print(type(b))
+
+# Python automatically converts 
+# c to float as it is a float addition 
+c = a + b 
+print(c) 
+print(type(c))
+ 
+# Python automatically converts 
+# d to float as it is a float multiplication
+d = a * b
+print(d)
+print(type(d))
+
+# int to float 
+f = 4
+x = float(f)
+print(type(x))
+
+# float to int 
+g = 2.7
+y = int(g)
+print(y, type(y))
+
+# int to string 
+pie = 3.142
+print(str(pie))
