b2d34afd3f3613c16a7d892ddcb2e91f6977bc0a
antonygatua <antonnymuiko@gmail.com>
2024-02-20 19:36:21 +0300

    Python type conversions

---
+++ The Basics/Files and Exceptions/typeconversion.py
@@ -0,0 +1,14 @@
+# example 1
+int_value = 123
+float_value = 12.34
+
+val = int_value + float_value
+print(val, type(val))
+
+# example 2
+int_value = 12
+string_value = '234'
+
+val = int(string_value) + int_value
+
+print(val,type(val))
